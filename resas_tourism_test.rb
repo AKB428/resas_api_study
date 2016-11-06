b93059a5bb1c65066b03a7eb7b3e8554d8e2ad51
AKB428 <gandara016+1@gmail.com>
2016-11-06 17:09:44 +0900

    add tour

---
+++ resas_tourism_test.rb
@@ -0,0 +1,33 @@
+require 'httpclient'
+require 'json'
+
+api_key = nil
+
+File.open './config/config.json' do |file|
+  conf = JSON.load(file.read)
+  api_key = conf['RESAS']['api_key']
+end
+
+END_POINT = 'https://opendata.resas-portal.go.jp'
+
+API_PATH = 'api/v1-rc.1/tourism/foreigners/forFrom'
+
+http_client = HTTPClient.new
+query = { 'year' => '2016', 'prefCode' => '08', 'purpose' => '1' }
+url = File.join(END_POINT, API_PATH)
+
+#https://opendata.resas-portal.go.jp/docs/api/v1-rc.1/detail/index.html
+auth_header = {
+    'X-API-KEY' => api_key
+}
+
+result = http_client.get_content(URI.parse(URI.encode(url)), query, auth_header)
+
+
+data = JSON.load(result)
+
+ data['result']['changes'].each do |d|
+  p d['countryName']
+end
+
+
